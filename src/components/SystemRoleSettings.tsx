06c6e27c4c49750a1bbcc967db8167d3b0ab140a
David Garcia <116240295+codavidgarcia@users.noreply.github.com>
2023-03-08 22:30:10 -0500

    Update SystemRoleSettings.tsx

--- src/components/SystemRoleSettings.tsx
+++ src/components/SystemRoleSettings.tsx
@@ -22,10 +22,10 @@ export default (props: Props) => {
     <div class="my-4">
       <Show when={!props.systemRoleEditing()}>
         <Show when={props.currentSystemRoleSettings()}>
-          <div>
-            <div class="flex items-center gap-1 op-50 dark:op-60">
+          <div class="text-slate">
+            <div class="flex items-center gap-1 op-60 text-slate">
               <IconEnv />
-              <span>System Role:</span>
+              <span>Comportamiento de AI:</span>
             </div>
             <div class="mt-1">
               { props.currentSystemRoleSettings() }
@@ -33,23 +33,23 @@ export default (props: Props) => {
           </div>
         </Show>
         <Show when={!props.currentSystemRoleSettings() && props.canEdit()}>
-          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="inline-flex items-center justify-center gap-1 text-sm bg-slate/20 px-2 py-1 rounded-md transition-colors cursor-pointer hover:bg-slate/50">
+          <span onClick={() => props.setSystemRoleEditing(!props.systemRoleEditing())} class="inline-flex items-center justify-center gap-1 text-sm text-slate bg-slate/20 px-2 py-1 rounded-md transition-colors cursor-pointer hover:bg-slate/50">
             <IconEnv />
-            <span>Add System Role</span>
+            <span>Añadir comportamiento de AI</span>
           </span>
         </Show>
       </Show>
       <Show when={props.systemRoleEditing() && props.canEdit()}>
         <div>
-          <div class="flex items-center gap-1 op-50 dark:op-60">
+          <div class="flex items-center gap-1 op-60 text-slate">
             <IconEnv />
-            <span>System Role:</span>
+            <span>Comportamiento de AI:</span>
           </div>
-          <p class="my-2 leading-normal text-sm op-50 dark:op-60">Gently instruct the assistant and set the behavior of the assistant.</p>
+          <p class="my-2 leading-normal text-slate text-sm op-60">Instruya al asistente y establece el comportamiento del asistente.</p>
           <div>
             <textarea
               ref={systemInputRef!}
-              placeholder="You are a helpful assistant, answer as concisely as possible...."
+              placeholder="Eres un asistente útil, responde de la manera más concisa posible."
               autocomplete="off"
               autofocus
               rows="3"
@@ -57,20 +57,21 @@ export default (props: Props) => {
               px-3 py-3 my-1
               min-h-12
               max-h-36
+              text-slate
               rounded-sm
               bg-slate
               bg-op-15
               focus:bg-op-20
               focus:ring-0
               focus:outline-none
-              placeholder:op-50
-              dark="placeholder:op-30"
+              placeholder:text-slate-400
+              placeholder:op-30
               overflow-hidden
               resize-none scroll-pa-8px
             />
           </div>
-          <button onClick={handleButtonClick} h-12 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm>
-            Set
+          <button onClick={handleButtonClick} h-12 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 text-slate rounded-sm>
+            Definir
           </button>
         </div>
       </Show>
